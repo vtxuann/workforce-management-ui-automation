c610df8d5e257966bb2a20e8626126d346e44765
Vo Thanh Xuan <vtxuan.work@gmail.com>
2026-09-19 13:29:36 +0700

    chore(test): stabilize regression execution

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,13 +6,15 @@ dotenv.config();
 export default defineConfig({
   testDir: './tests',
 
-  fullyParallel: true,
+  timeout: 60_000,
+
+  fullyParallel: false,
 
   forbidOnly: !!process.env.CI,
 
   retries: process.env.CI ? 2 : 0,
 
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 2,
 
   reporter: [['html', { open: 'never' }], ['list']],
 
@@ -23,12 +25,15 @@ export default defineConfig({
 
     screenshot: 'only-on-failure',
 
-    video: 'retain-on-failure',
+    video: 'off',
 
     headless: true,
   },
 
   projects: [
-    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
   ],
 });
